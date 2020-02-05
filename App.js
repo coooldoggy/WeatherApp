0840b40fe875fcd4a1fb7dfc682d8ac2fd044ca1
coooldoggy <coooldoggy@gmail.com>
2020-02-05 16:32:48 +0900

    Building the Loading View

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+    state = {
+        isLoaded: false
+    }
+
+    render() {
+        const {isLoaded} = this.state
+        return (
+            <View style={styles.container}>
+                {isLoaded ? null : (
+                    <View style={styles.Loading}>
+                        <Text style={styles.loadingText}>
+                            Getting the fucking weather
+                        </Text>
+                    </View>)}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    Loading: {
+      flex: 1,
+      backgroundColor: '#FDF6AA',
+      justifyContent: 'flex-end',
+      paddingLeft: 25,
+    },
+  loadingText:{
+      fontSize: 38,
+    marginBottom: 100,
+  }
+});
