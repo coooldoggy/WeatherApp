14dbd48dd451d0aa890b983c921e6fd3bda06c19
yulim <coooldoggy@gmail.com>
2020-02-08 15:27:24 +0900

    -Working with Icons

--- Weather.js
+++ Weather.js
@@ -1,22 +1,23 @@
 import React, {Component} from "react";
 import {StyleSheet, Text, View} from "react-native";
 import {LinearGradient} from "expo-linear-gradient";
+import {Ionicons} from "@expo/vector-icons";
 
 export default class Weather extends Component {
     render() {
         return (
-                <LinearGradient
-                    colors={["#00C6FB", "#005BEA"]}
-                    style={styles.container}>
-                        <View style={styles.upper}>
-                            <Text>Icon here!</Text>
-                            <Text style={styles.temp}>35°C</Text>
-                        </View>
-                        <View style={styles.lower}>
-                            <Text style={styles.title}>Raining like a MF</Text>
-                            <Text style={styles.subtitle}>For more info look outside</Text>
-                        </View>
-                </LinearGradient>
+            <LinearGradient
+                colors={["#00C6FB", "#005BEA"]}
+                style={styles.container}>
+                <View style={styles.upper}>
+                    <Ionicons color="white" size={144} name={"ios-rainy"}></Ionicons>
+                    <Text style={styles.temp}>35°C</Text>
+                </View>
+                <View style={styles.lower}>
+                    <Text style={styles.title}>Raining like a MF</Text>
+                    <Text style={styles.subtitle}>For more info look outside</Text>
+                </View>
+            </LinearGradient>
         );
     }
 }
@@ -26,31 +27,32 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     upper: {
-      flex : 1,
-      alignItems: "center",
-      justifyContent: "center"
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "transparent"
     },
-    temp:{
-        fontSize: 38,
+    temp: {
+        fontSize: 48,
         backgroundColor: "transparent",
         color: "white",
         marginBottom: 50,
         marginTop: 10
     },
-    lower:{
+    lower: {
         flex: 1,
         alignItems: "flex-start",
         justifyContent: "flex-end",
         paddingLeft: 25,
     },
-    title:{
+    title: {
         fontSize: 38,
         backgroundColor: "transparent",
         color: "white",
         marginBottom: 10,
         fontWeight: "300"
     },
-    subtitle:{
+    subtitle: {
         fontSize: 24,
         backgroundColor: "transparent",
         color: "white",
