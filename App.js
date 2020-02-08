1bb6ad7c25c1f158cce6ac327f62e481e3cb4749
yulim <coooldoggy@gmail.com>
2020-02-08 21:43:05 +0900

    -Getting the weather from API

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, StatusBar} from 'react-native';
-import Weather from "./Weather"
+import Weather from "./Weather";
+
+const API_KEY = "";
 
 export default class App extends Component {
     state = {
@@ -10,9 +12,7 @@ export default class App extends Component {
     componentDidMount() {
         navigator.geolocation.getCurrentPosition(
            position => {
-               this.setState({
-                   isLoaded: true
-               });
+               this._getWeather(position.coords.latitude, position.coords.longitude);
            },
             error =>{
                this.setState({
@@ -20,6 +20,12 @@ export default class App extends Component {
                })
             }
         );
+    }
+
+    _getWeather = (lat, long) => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+            .then(response => response.json())
+            .then(json => console.log(json))
     }
 
     render() {
